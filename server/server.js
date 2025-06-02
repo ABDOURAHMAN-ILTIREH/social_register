66eb114e3c7ac909a2429d846521270d6a899740
abdourahrahman Iltireh <arfanabdourahman@gmail.com>
2025-06-02 14:56:23 +0300

    set database connection

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("dotenv").config();
 const cookieParser = require("cookie-parser")
+const path = require('path');
 
 const cors = require('cors');
 
@@ -8,6 +9,10 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, '../client/dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
 
 const PORT = process.env.PORT || 5000;
 app.use(cors({
